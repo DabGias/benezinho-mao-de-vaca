656e98e4188570b4a7bbf1dfcea700b73e5ec4e3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-17 09:05:12 -0300

    commit inicial

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,11 +1,14 @@
 package br.com.fiap;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
+    final static EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("oracle-teste");
+    final static EntityManager MANAGER = FACTORY.createEntityManager();
 
     public static void main(String[] args) {
 
-
     }
-
-
 }
